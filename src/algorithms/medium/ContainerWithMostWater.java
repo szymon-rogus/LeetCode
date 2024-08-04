302c099dc7b614d917b2fde8c5982ae760e604c8
szymon-rogus <sszymek98@gmail.com>
2024-08-04 11:02:12 +0200

    ContainerWithMostWater

---
+++ src/algorithms/medium/ContainerWithMostWater.java
@@ -0,0 +1,24 @@
+package algorithms.medium;
+
+/**
+ * <a href="https://leetcode.com/problems/container-with-most-water">...</a>
+ * This solution beats 55.63% other Java submissions' memory complexity
+ * This solution beats 68.95% other Java submissions' runtime
+ */
+public class ContainerWithMostWater {
+    public static int maxArea(int[] height) {
+        int pointer1 = 0;
+        int pointer2 = height.length - 1;
+        int waterArea = 0;
+        while (pointer1 < pointer2) {
+            int currentArea = (pointer2 - pointer1) * Math.min(height[pointer1], height[pointer2]);
+            waterArea = Math.max(waterArea, currentArea);
+            if (height[pointer1] < height[pointer2]) {
+                pointer1++;
+            } else {
+                pointer2--;
+            }
+        }
+        return waterArea;
+    }
+}
