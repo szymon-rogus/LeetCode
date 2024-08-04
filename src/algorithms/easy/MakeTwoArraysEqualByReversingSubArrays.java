7325354adec7f3325e566eaf23428a4f882b05a5
szymon-rogus <sszymek98@gmail.com>
2024-08-04 10:23:27 +0200

    MakeTwoArraysEqualByReversingSubArrays

---
+++ src/algorithms/easy/MakeTwoArraysEqualByReversingSubArrays.java
@@ -0,0 +1,16 @@
+package algorithms.easy;
+
+import java.util.Arrays;
+
+/**
+ * <a href="https://leetcode.com/problems/make-two-arrays-equal-by-reversing-subarrays">...</a>
+ * This solution beats 24.11% other Java submissions' memory complexity
+ * This solution beats 54.32% other Java submissions' runtime
+ */
+public class MakeTwoArraysEqualByReversingSubArrays {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        Arrays.sort(arr);
+        Arrays.sort(target);
+        return Arrays.equals(arr, target);
+    }
+}
