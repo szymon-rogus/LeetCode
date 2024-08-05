82a79e23ada0441008bac7871cc0e734072ef5be
szymon-rogus <sszymek98@gmail.com>
2024-08-05 16:00:39 +0200

    MaximumSubArray

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import algorithms.medium.MaximumSubArray;
 
 public class Main {
     public static void main(String[] args) {
-        int [] list = new int[]{1,2,-1,-2,2,1,-2,1};
-        System.out.println(MaximumSubArray.maxSubArray(list));
+
     }
 }
---
+++ src/algorithms/medium/MaximumSubArray.java
@@ -0,0 +1,21 @@
+package algorithms.medium;
+
+/**
+ * <a href="https://leetcode.com/problems/maximum-subarray">...</a>
+ * This solution beats 13.11% other Java submissions' memory complexity
+ * This solution beats 99.56% other Java submissions' runtime
+ */
+public class MaximumSubArray {
+    public static int maxSubArray(int[] nums) {
+        int sum = nums[0];
+        int pointer = 0;
+        int localSum = 0;
+        while (pointer < nums.length) {
+            localSum += nums[pointer];
+            sum = Math.max(sum, localSum);
+            localSum = Math.max(0, localSum);
+            pointer++;
+        }
+        return sum;
+    }
+}
