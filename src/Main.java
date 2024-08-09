adb553a2710a3281d214246198dd7d792ceaf49a
szymon-rogus <sszymek98@gmail.com>
2024-08-09 16:49:23 +0200

    ValidParentheses

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import algorithms.medium.JumpGameII;
+
 public class Main {
     public static void main(String[] args) {
-
+        int [] nums = new int[]{2, 3, 1, 1, 4};
+        System.out.println(JumpGameII.jump(nums));
     }
 }
---
+++ src/algorithms/easy/ValidParentheses.java
@@ -0,0 +1,42 @@
+package algorithms.easy;
+
+import java.util.Stack;
+
+/**
+ * <a href="https://leetcode.com/problems/valid-parentheses/submissions">...</a>
+ * This solution beats 21.94% other Java submissions' memory complexity
+ * This solution beats 52.41% other Java submissions' runtime
+ */
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            }
+            if (c == ')') {
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+            if (c == '}') {
+                if (!stack.isEmpty() && stack.peek() == '{') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+            if (c == ']') {
+                if (!stack.isEmpty() && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
