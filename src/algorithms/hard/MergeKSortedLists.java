cb9e0303a371c411462500745b981bb89b9ae4af
szymon-rogus <sszymek98@gmail.com>
2024-08-10 11:43:58 +0200

    MergeKSortedLists

---
+++ src/algorithms/hard/MergeKSortedLists.java
@@ -0,0 +1,24 @@
+package algorithms.hard;
+
+import dataStrucutres.ListNode;
+import java.util.Arrays;
+import static algorithms.easy.MergeTwoSortedLists.mergeTwoLists;
+
+/**
+ * <a href="https://leetcode.com/problems/merge-k-sorted-lists">...</a>
+ * This solution beats 8.66% other Java submissions' memory complexity
+ * This solution beats 75.43% other Java submissions' runtime
+ */
+public class MergeKSortedLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
+        if (lists.length == 1) return lists[0];
+        if (lists.length == 2) return mergeTwoLists(lists[0], lists[1]);
+
+        ListNode[] one = Arrays.copyOfRange(lists, 0, lists.length / 2);
+        ListNode[] two = Arrays.copyOfRange(lists, lists.length / 2, lists.length);
+
+        // I used implementation from other exercise - the method can be transferred here
+        return mergeTwoLists(mergeKLists(one), mergeKLists(two));
+    }
+}
