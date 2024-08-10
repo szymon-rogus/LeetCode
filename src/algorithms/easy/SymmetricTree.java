b3f40baf000065d512ffb5f95ddcd211afa9f458
szymon-rogus <sszymek98@gmail.com>
2024-08-10 22:42:12 +0200

    SymmetricTree

---
+++ src/algorithms/easy/SymmetricTree.java
@@ -0,0 +1,26 @@
+package algorithms.easy;
+
+import dataStrucutres.TreeNode;
+
+/**
+ * <a href="https://leetcode.com/problems/symmetric-tree">...</a>
+ * This solution beats 90.39% other Java submissions' memory complexity
+ * This solution beats 100% other Java submissions' runtime
+ */
+public class SymmetricTree {
+    public static boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+
+        return isSymmetric(root.left, root.right);
+    }
+
+    private static boolean isSymmetric(TreeNode rootLeft, TreeNode rootRight) {
+        if (rootLeft == null && rootRight == null) return true;
+        if (rootLeft == null || rootRight == null) return false;
+
+        if (rootLeft.val == rootRight.val) {
+            return isSymmetric(rootLeft.left, rootRight.right) && isSymmetric(rootLeft.right, rootRight.left);
+        }
+        return false;
+    }
+}
