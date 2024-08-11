db943c624f23117daeb79978e5feda9c27460a18
szymon-rogus <sszymek98@gmail.com>
2024-08-11 15:24:42 +0200

    CountCompleteTreeNodes

---
+++ src/algorithms/easy/tree/CountCompleteTreeNodes.java
@@ -0,0 +1,18 @@
+package algorithms.easy.tree;
+
+import dataStrucutres.TreeNode;
+
+/**
+ * <a href="https://leetcode.com/problems/symmetric-tree">...</a>
+ * This solution beats 60.29% other Java submissions' memory complexity
+ * This solution beats 100% other Java submissions' runtime
+ */
+public class CountCompleteTreeNodes {
+
+    // This is the most basic problem when first dealing with Trees
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+}
