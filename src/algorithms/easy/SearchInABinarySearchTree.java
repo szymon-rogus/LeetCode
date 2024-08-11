014d20fbd6b48a9aac34ad3d56b0a7fd8db85697
szymon-rogus <sszymek98@gmail.com>
2024-08-11 10:54:53 +0200

    SearchInABinarySearchTree

---
+++ src/algorithms/easy/SearchInABinarySearchTree.java
@@ -0,0 +1,18 @@
+package algorithms.easy;
+
+import dataStrucutres.TreeNode;
+
+/**
+ * <a href="https://leetcode.com/problems/search-in-a-binary-search-tree">...</a>
+ * This solution beats 16.60% other Java submissions' memory complexity
+ * This solution beats 100% other Java submissions' runtime
+ */
+public class SearchInABinarySearchTree {
+    public static TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) return null;
+        if (root.val == val) return root;
+
+        if (root.val > val) return searchBST(root.left, val);
+        return searchBST(root.right, val);
+    }
+}
