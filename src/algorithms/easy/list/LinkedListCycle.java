3bd55a2018ff5cb441f28bd23806b78b7b29c5a8
szymon-rogus <sszymek98@gmail.com>
2024-08-12 12:53:58 +0200

    LinkedListCycle

---
+++ src/algorithms/easy/list/LinkedListCycle.java
@@ -0,0 +1,32 @@
+package algorithms.easy.list;
+
+import dataStrucutres.ListNode;
+
+/**
+ * <a href="https://leetcode.com/problems/linked-list-cycle">...</a>
+ * This solution beats 10.88% other Java submissions' memory complexity
+ * This solution beats 100% other Java submissions' runtime
+ */
+public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+
+        ListNode oneStep = new ListNode(head.val, head.next);
+        ListNode twoStep = new ListNode(head.val, head.next);
+
+        while (true) {
+            if (twoStep.next != null && twoStep.next.next != null) {
+                twoStep = twoStep.next.next;
+            } else {
+                return false;
+            }
+
+            if (oneStep.next != null) {
+                oneStep = oneStep.next;
+            }
+
+            if (oneStep == twoStep) return true;
+        }
+    }
+}
