2c999fc353456e93995336fd9c370f26781055c4
szymon-rogus <sszymek98@gmail.com>
2024-08-12 12:54:20 +0200

    BinaryTreePreorderTraversal

---
+++ src/algorithms/easy/tree/BinaryTreePreorderTraversal.java
@@ -0,0 +1,33 @@
+package algorithms.easy.tree;
+
+import dataStrucutres.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * <a href="https://leetcode.com/problems/binary-tree-preorder-traversal">...</a>
+ * This solution beats 62.44% other Java submissions' memory complexity
+ * This solution beats 100% other Java submissions' runtime
+ */
+public class BinaryTreePreorderTraversal {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new LinkedList<>();
+        if (root == null) return list;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            list.add(node.val);
+
+            if (node.right != null) stack.push(node.right);
+            if (node.left != null) stack.push(node.left);
+        }
+
+        return list;
+    }
+}
