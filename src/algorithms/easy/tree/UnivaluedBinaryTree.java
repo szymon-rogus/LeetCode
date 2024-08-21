470ec6fe7e3db91f3c399f83fc71ae4e58b98507
szymon-rogus <sszymek98@gmail.com>
2024-08-21 22:14:44 +0200

    UnivaluedBinaryTree

---
+++ src/algorithms/easy/tree/UnivaluedBinaryTree.java
@@ -0,0 +1,28 @@
+package algorithms.easy.tree;
+
+import dataStrucutres.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * <a href="https://leetcode.com/problems/univalued-binary-tree">...</a>
+ * This solution beats 28.57% other Java submissions' memory complexity
+ * This solution beats 100% other Java submissions' runtime
+ */
+public class UnivaluedBinaryTree {
+
+    public static boolean isUnivalTree(TreeNode root) {
+        int helper = root.val;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+
+            if (node.val != helper) return false;
+            if (node.left != null) stack.push(node.left);
+            if (node.right != null) stack.push(node.right);
+        }
+        return true;
+    }
+}
