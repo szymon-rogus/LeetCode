aae535ceace08299c57f06cbd90906b265610f16
szymon-rogus <sszymek98@gmail.com>
2025-04-05 22:38:24 +0200

    SmallestStringStartingFromLeaf

---
+++ src/algorithms/medium/tree/SmallestStringStartingFromLeaf.java
@@ -0,0 +1,30 @@
+package algorithms.medium.tree;
+
+import dataStrucutres.TreeNode;
+
+/**
+ * <a href="https://leetcode.com/problems/smallest-string-starting-from-leaf">...</a>
+ * This solution beats 52.40% other Java submissions' memory complexity
+ * This solution beats 89.78% other Java submissions' runtime
+ */
+public class SmallestStringStartingFromLeaf {
+
+    public String smallestFromLeaf(TreeNode root) {
+        return getSmaller(root, "");
+    }
+
+    private String getSmaller(TreeNode root, String string) {
+        if (root.left == null && root.right == null) return convert(root.val).concat(string);
+        if (root.left == null) return getSmaller(root.right, convert(root.val).concat(string));
+        if (root.right == null) return getSmaller(root.left, convert(root.val).concat(string));
+
+        String left = getSmaller(root.left, convert(root.val).concat(string));
+        String right = getSmaller(root.right, convert(root.val).concat(string));
+        return left.compareTo(right) > 0 ? right : left;
+    }
+
+    private String convert(int value) {
+        char element = (char) (value + 97);
+        return String.valueOf(element);
+    }
+}
