9b07289aae8ffc3aaa3b561566852a0ba6535d0c
szymon-rogus <sszymek98@gmail.com>
2025-04-07 22:00:34 +0200

    IsSubsequence

---
+++ src/algorithms/easy/IsSubsequence.java
@@ -0,0 +1,20 @@
+package algorithms.easy;
+
+/**
+ * <a href="https://leetcode.com/problems/is-subsequence">...</a>
+ * This solution beats 9.74% other Java submissions' memory complexity
+ * This solution beats 65.88% other Java submissions' runtime
+ */
+public class IsSubsequence {
+
+    public static boolean isSubsequence(String s, String t) {
+        int sPointer = 0;
+        for (int i = 0; i < t.length(); i++) {
+            ///  if we move to the end of the subString with pointer,
+            ///  it means all characters has been present in the original one
+            if (sPointer == s.length()) return true;
+            if (t.charAt(i) == s.charAt(sPointer)) sPointer++;
+        }
+        return sPointer == s.length();
+    }
+}
