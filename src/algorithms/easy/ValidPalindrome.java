b256fc49ca91cd8e8b5d77db218fdc3e6406cf60
szymon-rogus <sszymek98@gmail.com>
2025-04-07 21:30:35 +0200

    ValidPalindrome

---
+++ src/algorithms/easy/ValidPalindrome.java
@@ -0,0 +1,19 @@
+package algorithms.easy;
+
+/**
+ * <a href="https://leetcode.com/problems/valid-palindrome">...</a>
+ * This solution beats 30.00% other Java submissions' memory complexity
+ * This solution beats 33.59% other Java submissions' runtime
+ */
+public class ValidPalindrome {
+
+    public static boolean isPalindrome(String s) {
+        /// getting rid of the non-alphanumeric elements
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        for (int i = 0; i < s.length()/2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) return false;
+        }
+        return true;
+    }
+}
