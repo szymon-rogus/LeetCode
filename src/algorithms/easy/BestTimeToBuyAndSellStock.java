ec44d051dafa6720eb0e55323fd58b49115f59f6
szymon-rogus <sszymek98@gmail.com>
2025-04-08 13:59:36 +0200

    BestTimeToBuyAndSellStock

---
+++ src/algorithms/easy/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,25 @@
+package algorithms.easy;
+
+/**
+ * <a href="https://leetcode.com/problems/best-time-to-buy-and-sell-stock">...</a>
+ * This solution beats 96.58% other Java submissions' memory complexity
+ * This solution beats 99.81% other Java submissions' runtime
+ */
+public class BestTimeToBuyAndSellStock {
+
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int buyPointer = 0;
+
+        /// iterate over everyday
+        for (int i = 0; i < prices.length; i++) {
+
+            /// if we come up with better buy prices, we stick to it
+            if (prices[buyPointer] > prices[i]) buyPointer = i;
+
+            /// if we can get more profit, we update profit
+            if (prices[i] - prices[buyPointer] > profit) profit = prices[i] - prices[buyPointer];
+        }
+        return profit;
+    }
+}
