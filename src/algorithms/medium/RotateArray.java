c54459f4071c4f98185ed9593834bb85ab5ecd96
szymon-rogus <sszymek98@gmail.com>
2025-04-22 18:16:08 +0200

    RotateArray

---
+++ src/algorithms/medium/RotateArray.java
@@ -0,0 +1,26 @@
+package algorithms.medium;
+
+/**
+ * <a href="https://leetcode.com/problems/rotate-array">...</a>
+ * This solution beats 92.93% other Java submissions' memory complexity
+ * This solution beats 28.59% other Java submissions' runtime
+ */
+public class RotateArray {
+
+    /**
+     * Brutal solution O(n) runtime and memory
+     */
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        if (k == 0) return;
+
+        int[] rotated = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            int newIndex = (i + k) % nums.length;
+            rotated[newIndex] = nums[i];
+        }
+
+        System.arraycopy(rotated, 0, nums, 0, nums.length);
+    }
+}
