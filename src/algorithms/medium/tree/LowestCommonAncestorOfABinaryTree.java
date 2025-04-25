5d43d3b7392c944afd49ee85c7556b8835999042
szymon-rogus <sszymek98@gmail.com>
2025-04-25 22:12:18 +0200

    LowestCommonAncestorOfABinaryTree

---
+++ src/algorithms/medium/tree/LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,24 @@
+package algorithms.medium.tree;
+
+import dataStructures.TreeNode;
+
+/**
+ * <a href="https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree">...</a>
+ * This solution beats 73.07% other Java submissions' memory complexity
+ * This solution beats 63.66% other Java submissions' runtime
+ */
+public class LowestCommonAncestorOfABinaryTree {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        /// if we come to the end there is key in this subtree
+        if (root == null) return null;
+        /// we found one of the keys
+        if (root.val == p.val || root.val == q.val) return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) return root;
+        return left != null ? left : right;
+    }
+}
