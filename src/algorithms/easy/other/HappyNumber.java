ea09fbce6c6546a1bd1c915a9eb9900021f12d15
szymon-rogus <sszymek98@gmail.com>
2025-05-18 17:10:29 +0200

    HappyNumber

---
+++ src/algorithms/easy/other/HappyNumber.java
@@ -0,0 +1,30 @@
+package algorithms.easy.other;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.com/problems/happy-number">...</a>
+ * This solution beats 39.69% other Java submissions' memory complexity
+ * This solution beats 66.84% other Java submissions' runtime
+ */
+public class HappyNumber {
+
+    public boolean isHappy(int n) {
+        List<Integer> list = new ArrayList<>();
+
+        while (n != 1) {
+            if (list.contains(n)) return false;
+
+            list.add(n);
+            int next = 0;
+            while (n > 0) {
+                next += (n % 10) * (n % 10);
+                n /= 10;
+            }
+            n = next;
+
+        }
+        return true;
+    }
+}
