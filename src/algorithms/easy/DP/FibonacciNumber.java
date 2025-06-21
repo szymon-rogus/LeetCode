256b65bf050c90f8e8770103e39330e2cff3b133
szymon-rogus <sszymek98@gmail.com>
2025-06-21 19:51:36 +0200

    FibonacciNumber

---
+++ src/algorithms/easy/DP/FibonacciNumber.java
@@ -0,0 +1,29 @@
+package algorithms.easy.DP;
+
+/**
+ * <a href="https://leetcode.com/problems/majority-element">...</a>
+ * This solution beats 100.00% other Java submissions' runtime
+ * This solution beats 11.59% other Java submissions' memory complexity
+ */
+public class FibonacciNumber {
+
+    /**
+     * Optimal DP approach with memoization of 2 most recent elements
+     * @param n fibonacci number
+     * @return result of fibonacci sequence
+     */
+    public int fib(int n) {
+        if (n == 0 || n == 1) return n;
+
+        int current = 1;
+        int previous = 0;
+
+        for (int i = 2; i <= n; i++) {
+            int next = current + previous;
+            previous = current;
+            current = next;
+        }
+
+        return current;
+    }
+}
